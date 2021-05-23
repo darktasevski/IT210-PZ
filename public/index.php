0420b50f61404ac6807732f91180062550cdf9b1
Darkø Tasevski <darko.tasevski@redbox.com>
2021-05-23 13:48:07 +0200

    feature: product page

--- public/index.php
+++ public/index.php
@@ -30,12 +30,36 @@ define('ROOT_PATH', __DIR__);
     <img class="hero__alt" src="./images/gugnir_1.png" alt="Gugnir image">
 </div>
 <main>
-    <section class="hero__message">
-        <p>Jewelry is something that reflects the essence of a human being during the tens of thousands of years.</p>
-        <p><em> Since ancient times, people wear pendants, amulets, bracelets and rings... <span class="h__underline">These pieces show people's individuality,</span>
-            nationality, <span class="h__underline">religious beliefs, and social status.</span>
-            They give protection, inspire confidence and, of course, simply adorn men and women.</em></p>
-        <p class="link"><i>ß </i><a href="pages/about.php">About Us</a></p>
+    <section class="hero">
+        <section class="hero__message">
+            <p>Jewelry is something that reflects the essence of a human being during the tens of thousands of years.</p>
+            <p><em> Since ancient times, people wear pendants, amulets, bracelets and rings... <span class="h__underline">These pieces show people's individuality,</span>
+                    nationality, <span class="h__underline">religious beliefs, and social status.</span>
+                    They give protection, inspire confidence and, of course, simply adorn men and women.</em></p>
+            <p class="link"><i>ß </i><a href="pages/about.php">About Us</a></p>
+        </section>
+    <p class="arrow_down">&#8964;</p>
+    </section>
+
+
+    <section class="featured">
+        <h2 class="featured__title">Featured products</h2>
+        <div class="featured__container">
+            <?php
+                require_once('../src/common/connection.php');
+                $conn = connect();
+
+                $sql = "SELECT * FROM jewelry ORDER BY RAND() LIMIT 4";
+                $result = $conn->query($sql);
+
+                while ($row = $result->fetch()) {
+                    echo '<div class="featured__item">
+                        <img class="featured__item__img" id="'.$row["id"].'" src="'.$row["image_uri"].'" alt="image" />
+                        <p class="featured__item__title">'. $row["name"] .'</p>
+                        </div>';
+                }
+            ?>
+        </div>
     </section>
 </main>
 
@@ -49,6 +73,16 @@ define('ROOT_PATH', __DIR__);
         </div>';
     }
 ?>
+
+<script>
+    const featured = document.getElementsByClassName("featured__container")[0];
+
+    featured.addEventListener('click', function (e) {
+        if (e.target.id) {
+            window.location.href = `pages/product.php?id=${e.target.id}`;
+        }
+    })
+</script>
 
 </body>
 </html>
---
+++ public/pages/product.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Runestone | About Us</title>
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="../styles/normalize.css">
+    <link rel="stylesheet" href="../styles/fonts.css">
+    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="../styles/product.css">
+</head>
+<body>
+<?php require_once("../navigation.php") ?>
+
+<main>
+    <section class="about">
+        <?php
+            if (isset($_GET["id"]) && filter_var($_GET["id"], FILTER_VALIDATE_INT)){
+                require_once('../../src/common/connection.php');
+                $conn = connect();
+
+                $stmt = $conn->prepare("SELECT * FROM jewelry WHERE id=?");
+                $stmt->execute([$_GET["id"]]);
+                $row = $stmt->fetch();
+
+                if ($row) {
+                    echo '<div class="product">
+                        <h2 class="product__title">'.$row["name"].'</h2>
+                        <div class="product_layout">                      
+                            <img src="'.$row["image_uri"].'" alt="image" class="product__image" />
+                            <div>
+                                <p class="product__description">'.$row["description"].'</p>
+                                <p class="product__price">Price: '.$row["price"].'&#8364;</p>
+                            </div>
+                        </div>
+                    </div>';
+                } else {
+                    header("Location: ../index.php");
+                }
+            } else {
+                header("Location: ../index.php");
+            }
+        ?>
+    </section>
+
+</main>
+</body>
+</html>
